d1fc656afdc2c261d097f726a7d83ae181b11ff7
roman <roman.ecm@mail.ru>
2024-01-23 16:40:04 +0500

    Changed serializers and views

--- currency/serializers.py
+++ currency/serializers.py
@@ -3,7 +3,26 @@ from .models import Currency
 
 
 class CurrencySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Currency
-        fields = ('id', 'code', 'name', 'rate', 'date')
+        fields = ('id', 'code', 'name', 'rate', 'date', )
         read_only_fields = ('id',)
+
+
+class CombinedCurrencySerializer(serializers.Serializer):
+    latest_currency = serializers.SerializerMethodField()
+    last_10_requests = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ('latest_currency', 'last_10_currencies',)
+
+    def get_latest_currency(self, obj):
+        latest_currency = Currency.objects.latest('id')
+        serializer = CurrencySerializer(latest_currency)
+        return serializer.data
+
+    def get_last_10_requests(self, obj):
+        last_10_requests = Currency.objects.order_by('-id')[:10]
+        serializer = CurrencySerializer(last_10_requests, many=True)
+        return serializer.data
--- currency/views.py
+++ currency/views.py
@@ -2,12 +2,12 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Currency
-from .serializers import CurrencySerializer
+from .serializers import CurrencySerializer, CombinedCurrencySerializer
 
 
 class CurrencyView(viewsets.ModelViewSet):
-    serializer_class = CurrencySerializer
+    serializer_class = CombinedCurrencySerializer
 
     def get_queryset(self):
-        return Currency.objects.all().order_by('-date')[:10]
+        return Currency.objects.all().order_by('-id')[:1]
 
